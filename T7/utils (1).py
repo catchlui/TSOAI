e9df639d2670c8a3ff273e8c684cca46a60de062
snehashis <catchlui@gmail.com>
2023-06-17 08:51:44 +0530

    Delete utils (1).py

--- T7/utils (1).py
+++
@@ -1,7 +0,0 @@
-## This function checks the accuracy of the prediction
-from torchsummary import summary
-def GetCorrectPredCount(pPrediction, pLabels):
-  return pPrediction.argmax(dim=1).eq(pLabels).sum().item()
-
-def display_model_summary(model,input_structure=(1,28,28)):
-  summary(model, input_size=input_structure)
